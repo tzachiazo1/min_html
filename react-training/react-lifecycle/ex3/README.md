b2d5d039db748272d54ae4b0b71b8d61bc67dca6
Nir Moav <nirm@wix.com>
2017-06-18 21:53:23 +0300

    React Lifecycle - Ex3 - scaffolding simple app

---
+++ react-training/react-lifecycle/ex3/README.md
@@ -0,0 +1,12 @@
+## React LifeCycle - Exercise 3
+componentDidMount & componentWillUnmount
+
+
+# Guide
+1. Fork the Git Repo
+2. cd into this director (../react-training/react-lifecycle/ex4)
+3. run: yarn 
+4. run: yarn start
+5. solve the exercise:
+
+    
---
+++ react-training/react-lifecycle/ex3/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Exercise 3 - Implement!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
